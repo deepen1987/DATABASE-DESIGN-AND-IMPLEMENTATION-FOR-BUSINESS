123a92bcce9fd6ea1ffbf9e0d745e32b02f7442b
Deependrasingh Shekhawat <deepen1987@gmail.com>
2021-07-22 23:57:14 -0400

    commit

--- LearningPath/Scripts/STORED_PROC.sql
+++ LearningPath/Scripts/STORED_PROC.sql
@@ -91,9 +91,34 @@ END$$
 DELIMITER ;	
 
 
+SELECT 
+	client_id,
+    name,
+    get_risk_factor_for_client(client_id)
+from clients;
 
+drop function get_risk_factor_for_client;
 
+select invoice_total from invoices;
 
+CREATE FUNCTION `get_risk_factor_for_client`(
+	client_id int
+) RETURNS int
+    READS SQL DATA
+BEGIN
+	DECLARE risk_factor DECIMAL(9,2) DEFAULT 0;
+    DECLARE invoices_total DECIMAL(9,2);
+    DECLARE invoices_count INT;
+    
+    SELECT COUNT(*), SUM(i.invoice_total)
+    INTO invoices_count, invoices_total
+    FROM invoices i 
+    WHERE i.client_id = client_id;
+    
+    SET risk_factor = invoices_total / invoices_count * 5;
+
+	RETURN IFNULL(risk_factor,0);
+END
 
 
 
