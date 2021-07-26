a4dc4dab8d687b2b5f803a31d1f77e8a63d0bee2
Deependrasingh Shekhawat <deepen1987@gmail.com>
2021-07-26 18:54:23 -0400

    commit

--- Class/Labs/4/section1.sql
+++ Class/Labs/4/section1.sql
@@ -27,6 +27,9 @@ CREATE TABLE IF NOT EXISTS `likes`
     CONSTRAINT `fk_post_id` FOREIGN KEY(`post_id`) REFERENCES `post`(`post_id`)
 );
 
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
 INSERT INTO `person`
 VALUES
 		(
@@ -62,59 +65,59 @@ VALUES
         
 SELECT * FROM person;
 
-insert into `post`
-values
+INSERT INTO `post`
+VALUES
 		(
-		default,
+		DEFAULT,
         1,
         'WWE had a huge influence over my life',
         '2011-02-02',
         'WWE had a hu...'
         ),
         (
-		default,
+		DEFAULT,
         1,
         'WWE made a star out of me thank you for your support',
         '2012-02-02',
         'WWE made a s...'
         ),
         (
-		default,
+		DEFAULT,
         3,
         'Buckys role was excellently played by...',
         '2011-02-02',
         'Buckys role ...'
         ),
         (
-		default,
+		DEFAULT,
         3,
         'Hail Hydra was a quick thought in last avenger',
         '2020-02-02',
         'Hail hydra w...'
         ),
         (
-		default,
+		DEFAULT,
         4,
         'Avengers was the best movies series I worked in',
         '2021-02-02',
         'Avengers was...'
         ),
         (
-		default,
+		DEFAULT,
         4,
         'The character of Black Widow was a tough one',
         '2018-02-02',
         'The characte...'
         ),
         (
-		default,
+		DEFAULT,
         5,
         'Boston is one of the oldest cities in US',
         '2019-02-02',
         'Boston is on...'
         ),
         (
-		default,
+		DEFAULT,
         5,
         'Boston has the best beer in New England',
         '2019-02-02',
@@ -122,37 +125,196 @@ values
         );
         
 	
-select * from post;
+SELECT * FROM post;
 
 
-insert into likes
-values 
+INSERT INTO likes
+VALUES 
 		(
-		default,
+		DEFAULT,
         2,
         1,
         '2011-03-03'
         ),
         (
-		default,
+		DEFAULT,
         1,
         4,
         '2020-03-03'
         ),
         (
-		default,
+		DEFAULT,
         4,
         8,
         '2019-03-03'
         ),
         (
-		default,
+		DEFAULT,
         4,
         6,
         '2018-03-03'
         );
 
+SELECT * FROM likes;
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+drop procedure if exists add_zana_sage;
+delimiter $$
+create procedure add_zana_sage()
+begin
+insert into person
+values
+		(
+        6,
+        'Zana',
+        'Sage',
+        'z_sage'
+        );
+end$$
+delimiter;
+
+call add_zana_sage();
+
+select * from person;
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+call add_zana_sage();
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+DROP procedure IF EXISTS `add_person`;
+DELIMITER $$
+CREATE PROCEDURE `add_person` 
+(
+	f_name varchar(32),
+    l_name varchar(32),
+    u_name varchar(20)
+)
+BEGIN
+insert into person 
+values
+		(
+        default,
+		f_name,
+        l_name,
+        u_name
+        );
+END$$
+
+DELIMITER ;
+
+call add_person('The','Rock','t_rock');
+
+select * from person;
+
+delete from person where person_id = 8;
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+DROP PROCEDURE add_post;
+DELIMITER $$
+CREATE PROCEDURE add_post
+(
+	p_id INT,
+    contents VARCHAR(255),
+    c_date DATE
+)
+BEGIN
+	DECLARE agg_summary VARCHAR(15);
+	SET agg_summary = CONCAT(SUBSTRING(contents,1,12),'...');
+INSERT INTO post
+VALUES 
+		(
+        DEFAULT,
+        p_id,
+        contents,
+        c_date,
+        agg_summary
+        );
+END$$
+
+DELIMITER ;
+CALL add_post(5,'Cheked in at Hawaii...','2020-05-05');
+select * from post;
+
+delete from post where post_id = 9;
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+DROP PROCEDURE IF EXISTS add_like
+DELIMITER $$
+CREATE PROCEDURE add_like
+(
+	user_name VARCHAR(20),
+    post INT,
+    liked DATE
+)
+BEGIN
+	DECLARE p_id INT;
+    SELECT person_id 
+    INTO p_id
+    FROM person 
+    WHERE username = user_name;
+    INSERT INTO likes
+    VALUES 
+			(
+            DEFAULT,
+            p_id,
+            post,
+            liked
+            );
+END$$
+DELIMITER ;
+CALL add_like('z_sage', 8, '2021-07-25');
 select * from likes;
+
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+DROP PROCEDURE delete_person;
+DELIMITER $$
+CREATE PROCEDURE delete_person
+(
+	user_name VARCHAR(20)
+)
+BEGIN
+	DECLARE p_id INT;
+    SELECT person_id 
+    INTO p_id
+    FROM person WHERE username = user_name;
+    DELETE FROM likes WHERE person_id = p_id OR post_id IN (SELECT post_id FROM post WHERE person_id = p_id);
+    DELETE FROM post WHERE person_id = p_id;
+    DELETE FROM person WHERE person_id = p_id;
+END$$
+DELIMITER ;
+
+CALL delete_person('john5678');
+
+rollback;
+
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Class/Labs/4/section2.sql
@@ -0,0 +1,120 @@
+DROP TRIGGER IF EXISTS check_summary;
+
+DELIMITER $$
+CREATE TRIGGER check_summary
+	BEFORE INSERT ON post
+    FOR EACH ROW
+BEGIN
+	DECLARE post_summ VARCHAR(15);
+	SET post_summ = CONCAT(SUBSTR(new.content,1,12),'...');
+	IF NEW.summary <> post_summ THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Summary is not correct';
+    END IF;
+END $$
+DELIMITER ;
+
+INSERT INTO post
+VALUES 
+		(
+        DEFAULT,
+        4,
+        'Boston has the best beer in New England',
+        '2018-01-01',
+        'Boston has.t...'
+        );
+        
+INSERT INTO post
+VALUES 
+		(
+        DEFAULT,
+        4,
+        'Boston has the best beer in New England',
+        '2018-01-01',
+        'Boston has t...'
+        );
+        
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+DROP TRIGGER IF EXISTS verify_liked_on;
+
+DELIMITER $$
+CREATE TRIGGER verify_liked_on
+	BEFORE INSERT ON `likes`
+    FOR EACH ROW
+BEGIN
+	DECLARE create_date DATE;
+    
+    SELECT created_on 
+    INTO create_date 
+    FROM post
+    WHERE post_id = new.post_id;
+    
+    IF NEW.liked_on < create_date THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Liked on Date cannot be before the creation date';
+    END IF;
+END $$
+DELIMITER ;
+
+INSERT INTO likes
+VALUES		(        DEFAULT, 4, 11, '2017-01-01'        );
+        
+INSERT INTO likes
+VALUES		(        DEFAULT, 4, 11, '2019-01-01'        );
+
+
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS post_content_history;
+CREATE TABLE IF NOT EXISTS post_content_history
+(
+		history_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        old_content VARCHAR(255) NOT NULL,
+        action_type VARCHAR(50),
+        action_date DATETIME
+);
+
+SELECT * FROM post_content_history;
+
+DROP TRIGGER IF EXISTS content_update_history;
+
+DELIMITER $$
+CREATE TRIGGER content_update_history
+	AFTER UPDATE ON post
+    FOR EACH ROW
+BEGIN
+	INSERT INTO post_content_history
+    VALUES (DEFAULT, OLD.content, 'UPDATE', NOW());
+END $$
+DELIMITER ;
+
+UPDATE post
+SET  content = 'Checked in at Germany'
+WHERE post_id = 11;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
